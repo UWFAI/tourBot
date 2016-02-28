dbab5b9acbbfceeba8805de8fe9200088bf61dfd
joshua allen <joshuaallen64@gmail.com>
2016-02-27 21:39:56 -0600

    octree

---
+++ workspace/Octree/src/Octree.java
@@ -0,0 +1,17 @@
+
+public class Octree {
+	
+	private class BasicNode {}
+	
+	private class RootNode extends BasicNode {
+		
+	}
+	
+	private class BranchNode extends BasicNode {
+		
+	}
+	
+	private class LeafNode extends BasicNode {
+		
+	}
+}
