0c165ee5751c110f8352d6433afeb8bbb3274ba0
AIRG-tourbot <airg.tourbot@gmail.com>
2016-02-28 12:15:52 -0600

    javadoc stuff

--- workspace/RobotComm/src/IRobot.java
+++ workspace/RobotComm/src/IRobot.java
@@ -4,9 +4,12 @@ import jssc.*;
 /**
 * <h1>IRobot Communication</h1>
 * The IRobot class acts as the middle man between
-* the main AI and the IRobot. The java simple serial 
+* the main navigation AI and the IRobot. The java simple serial 
 * connector is used for the communication to make
 * the whole thing as simple as possible.
+* 
+* @author AIRG
+* @version 1.0.0.1
 */
 public class IRobot{
 	
@@ -15,6 +18,11 @@ public class IRobot{
 	private byte[] bytes = new byte[256];
 	private int[] senserData = new int[51];
 	
+	/**
+	* <h2>IRobot - Constructor</h2>
+	* The IRobot class constructor
+	* 
+	*/
 	public IRobot(){
 		
 		//
@@ -23,12 +31,18 @@ public class IRobot{
 			System.out.println(ports[i]);
 		}
 		
-		// a small speed up helper
+		/**
+		 * <p>
+		 * a small speed up helper
+		 */
 		for(int i=0; i<bytes.length; i++) {
 			bytes[i] = (byte) (i & 0xFF);
 		}
 		
-		// start the connection
+		/**
+		 * <p>
+		 * start the connection
+		 */ 
 		try {
 			sPort = new SerialPort("COM3");
 			sPort.openPort();
@@ -37,7 +51,12 @@ public class IRobot{
 			e.printStackTrace();
 		}
 		
-		// start the giving power to the kinect
+		/**
+		 * <p>
+		 * 
+		 * The iRobot's fan motor is activated.  This motor is modified to a power DC source to the XBOX One Kinect. 
+		 *  
+		*/
 		IO_start();
 		opcodes.add(131);
 		opcodes.add(144);
@@ -55,7 +74,10 @@ public class IRobot{
 		opcodes.add(0);
 		IO_send();
 		
-		// add an event listener to the port
+		/**
+		 * <p>
+		 * add an event listener to the port
+		 */
 		try {
 			sPort.addEventListener(new SerialPortEventListener() {
 				public void serialEvent(SerialPortEvent event) {
@@ -67,7 +89,13 @@ public class IRobot{
 		}
 		
 	}
-	
+
+	/**
+	 * <h2>listenerEvent</h2>
+	 * Event Listener 
+	 * 
+	 * @param event A Serial Port Event.
+	 */	
 	private void listenerEvent(SerialPortEvent event){
         if(event.isRXCHAR()){//If data is available
         	try {
@@ -93,7 +121,13 @@ public class IRobot{
             }
         }
 	}
-	//
+
+	/**
+	 * <h2>parseInput</h2>
+	 * add an event listener to the port
+	 * 
+	 * @param b A byte array.
+	 */	
 	private void parseInput(byte[] b){
 		byte header = b[0]; // I keep geting 13, I don't really know why...
 		byte nBytes = b[1]; //the number of bytes between the n-bytes byte and the checksum.
@@ -106,8 +140,9 @@ public class IRobot{
 	}
 
 	/**
-	* This method is used to send the list of commands 
-	* and clear the opcode list. 
+	* <h2>IO_send</h2>
+	* Sends the list of commands to the iRobot using a for loop. Opcode list is cleared at the end of the function.
+	* 
 	*/
 	public void IO_send(){
 		try {
@@ -119,17 +154,40 @@ public class IRobot{
 		}
 		opcodes.clear();
 	}
-
+	
+	/**
+	* <h2>IO_start</h2>
+	* Must be put into IO before sending data.
+	* 
+	*/
 	public void IO_start(){
 		opcodes.add(128);
 	}
+	
+	/**
+	* <h2>IO_reset</h2>
+	* Reset the IO session. Used for testing purposes.
+	* 
+	*/
 	public void IO_reset(){
 		opcodes.add(7);
 	}
+	
+	/**
+	* <h2>IO_stop</h2>
+	* Stop the IO session. Used for testing purposes.
+	* 
+	*/
 	public void IO_stop(){
 		opcodes.add(173);
 	}
 
+	/**
+	* <h2>IO_add</h2>
+	* Passes an integer array of opcodes. Function allows testing of opcodes. 
+	* 
+	* @param buffer An integer array.
+	*/
 	// an easy way to test commands
 	public void IO_add(int[] buffer){
 		for(int i = 0;i<buffer.length;i++){
@@ -137,6 +195,12 @@ public class IRobot{
 		}
 	}
 
+	/**
+	* <h2>setMode</h2>
+	* Activate or disable "Safe" mode.  This is for cliff detection.
+	* 
+	* @param safe A boolean variable.
+	*/
 	public void setMode(boolean safe){
 		if (safe)
 			opcodes.add(131);
@@ -151,6 +215,14 @@ public class IRobot{
 	// DrivePWM(Right PWM, Left PWM)
 	// LEDs // there are 4
 	// Digit_LEDs(d1, d2, d3)
+	
+	/**
+	* <h2>setSong</h2>
+	* Sends the list of commands to the iRobot using a for loop. Opcode list is cleared at the end of the function.
+	* 
+	* @param songIndex An integer variable.
+	* @param song Why the hell are you singing a song with a tour robot(an integer array)
+	*/
 	public void setSong(int songIndex, int[] song){
 		opcodes.add(140);
 		opcodes.add(songIndex);
@@ -159,24 +231,50 @@ public class IRobot{
 			opcodes.add(song[i]);
 		}
 	}
+
+	/**
+	* <h2>playSong</h2>
+	* Specifies the song to be played 
+	* 
+	* @param songIndex An integer variable.
+	*/
 	public void playSong(int songIndex){
 		opcodes.add(141);
 		opcodes.add(songIndex);
 	}
 	
 	// TODO: Input Commands
+
+	/**
+	* <h2>sensor_update</h2>
+	* Input Commands
+	* 
+	*/
 	public void sensors_update(){
 		opcodes.add(148);
 		opcodes.add(1);
 		opcodes.add(54);
 	}
-	
+
+	/**
+	* <h2>setDistance</h2>
+	* This also changes the detection distance.
+	* 
+	*/
 	// Distance - Packet ID: 19  - Data Bytes: 2, signed
 	private int distance = 0;
 	private void setDistance(int lByte, int rByte){
 		int val = ((lByte & 0xFF) << 8) | (rByte & 0xFF);
 		distance += val;
 	}
+
+	/**
+	* <h2>getDistance</h2>
+	* Returns the distance. 
+	* 
+	* @return dis An integer value.
+	* 
+	*/
 	public int getDistance(){
 		int dis = distance;
 		//distance = 0;
